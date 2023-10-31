78243924f6a536489afb6485fe4b11014dd0b5ff
Chinmay2808 <147637930+Chinmay2808@users.noreply.github.com>
2023-10-31 11:34:35 +0530

    Add files via upload

---
+++ new.sql
@@ -0,0 +1,15 @@
+-- Database: music_database
+
+-- DROP DATABASE IF EXISTS music_database;
+
+CREATE DATABASE music_database
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+	select * from employee
